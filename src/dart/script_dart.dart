3eb77a40e947e12b36212beee35b9ea7f8571572
keiyakeiya <75249966+keiyakeiya@users.noreply.github.com>
2025-12-22 23:49:16 +0900

    fix title

--- src/dart/script_dart.dart
+++ src/dart/script_dart.dart
@@ -4,7 +4,7 @@ import 'dart:html';
 void html_setup() {
   final header_elem = DivElement();
   header_elem?.setAttribute('id', 'header');
-  final title_elem = DivElement()..text = 'モンティホール問題';
+  final title_elem = DivElement()..text = '【たくさん】モンティホール問題';
   title_elem?.setAttribute('id', 'title');
   header_elem?.append(title_elem);
 
